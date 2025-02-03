7d60ee2ebd04ba4354627c21bd34ae27e4880755
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-02-03 09:13:25 -0500

    Serialization(Tutorial 1)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -21,20 +21,29 @@ Including another URLconf
 #     path('admin/', admin.site.urls),
 # ]
 
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+# from django.contrib import admin
+# from django.urls import include, path
+# from rest_framework import routers
 
-from quickstart import views
+# from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('snippets.urls')),
 ]
